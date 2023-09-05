d5ddea985011a7f3b28838dd81374d3486942d28
MD Raihan Hossain <73426725+mdraihanhossain07@users.noreply.github.com>
2023-09-06 02:35:36 +0600

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+jQuery(document).ready(function() {
+ 
+	jQuery(".et_pb_section_video_bg video").prop('volume', 1);    
+	jQuery(".et_pb_section_video_bg video").prop('muted', false); 
+
+  
+});
+	
+</script>
